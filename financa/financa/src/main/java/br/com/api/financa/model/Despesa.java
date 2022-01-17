5d35ec0f2251f7bf398ee8f521a9dfac186becc1
Marco Santana <marcos.sinsan@gmail.com>
2022-01-17 18:26:39 -0300

    Criando as classe model

---
+++ financa/financa/src/main/java/br/com/api/financa/model/Despesa.java
@@ -0,0 +1,64 @@
+package br.com.api.financa.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Despesa {
+	
+	private Long id;
+	private String descricao;
+	private Double valor;
+	private LocalDateTime data;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "Despesa [id=" + id + ", descricao=" + descricao + ", valor=" + valor + ", data=" + data + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(data, descricao, id, valor);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Despesa other = (Despesa) obj;
+		return Objects.equals(data, other.data) && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(id, other.id) && Objects.equals(valor, other.valor);
+	}
+	
+	
+	
+	
+}
