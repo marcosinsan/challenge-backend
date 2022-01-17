8c6bc31af2027ae7bf57170510163d83938252a7
Marco Santana <marcos.sinsan@gmail.com>
2022-01-17 19:37:18 -0300

    criando classe ReceitaDto

---
+++ financa/financa/src/main/java/br/com/api/financa/controller/dto/ReceitaDto.java
@@ -0,0 +1,44 @@
+package br.com.api.financa.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.api.financa.model.Receita;
+
+public class ReceitaDto {
+	
+	private Long id;
+	private String descricao;
+	private Double valor;
+	private LocalDateTime dataCriacao;
+	
+	public ReceitaDto(Receita receita) {
+		
+		this.id = receita.getId();
+		this.descricao = receita.getDescricao();
+		this.valor = receita.getValor();
+		this.dataCriacao = receita.getDataCriacao();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public static List<ReceitaDto> converte(List<Receita> receita) {
+		return receita.stream().map(ReceitaDto::new).collect(Collectors.toList());
+	}
+	
+}
