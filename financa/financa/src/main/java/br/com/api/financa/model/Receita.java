4d2bed2b6b12699bea71f0ac2a5ac0671ade3c5b
Marco Santana <marcos.sinsan@gmail.com>
2022-01-17 19:06:03 -0300

    Deletando arquivos.

--- financa/financa/src/main/java/br/com/api/financa/model/Receita.java
+++ financa/financa/src/main/java/br/com/api/financa/model/Receita.java
@@ -8,7 +8,12 @@ public class Receita {
 	private Long id;
 	private String descricao;
 	private Double valor;
-	private LocalDateTime data;
+	private LocalDateTime data = LocalDateTime.now();
+	
+	public Receita(String descricao, Double valor) {
+		this.descricao = descricao;
+		this.valor = valor;
+	}
 	
 	public Long getId() {
 		return id;
